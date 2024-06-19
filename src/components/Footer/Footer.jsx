b962cbe2bcfe765b7f9b689abc6750b8085dcc53
Facundo Arebalo <facuar1589@gmail.com>
2024-06-19 12:23:01 -0300

    footer, iconos redes sociales y estilo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import './footerStyle.css'
+import { IoLogoInstagram } from "react-icons/io";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+
+
+
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer-border'>
+            <Container className='footer-container'>
+                <Row>
+                    <Col className='help-column'>
+                        <h2>Ayuda</h2>
+                        <a href=''>About</a>
+                        <p>Preguntas frecuentes</p>
+                        <p>Términos y condiciones</p>
+                        <a href="">Contacto</a>
+                    </Col>
+                </Row>
+                <Row>
+                    <h2 className='text-center'>Social</h2>
+                    <Col className='social-icons'>
+                        <a href=''><FaFacebookF />
+                        </a>
+                        <a href=''><FaXTwitter />
+                        </a>
+                        <a href=''><IoLogoInstagram />
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
 
