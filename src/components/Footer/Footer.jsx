762fa3ba4bea5b062ccdb3303069096e708e20f0
Facundo Arebalo <facuar1589@gmail.com>
2024-06-25 12:32:37 -0300

    view about, test de registro, y pequeños cambios en footer y carrusel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,19 +3,21 @@ import './footerStyle.css'
 import { IoLogoInstagram } from "react-icons/io";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 const Footer = () => {
+    const navigate = useNavigate()
     return (
         <footer className='footer-border'>
             <Container className='footer-container'>
                 <Row>
                     <Col className='help-column'>
                         <h2>Ayuda</h2>
-                        <a href=''>About</a>
-                        <p>Preguntas frecuentes</p>
+                        <a onClick={() => navigate("/about")}>About</a>
+                        <p className='mt-3'>Preguntas frecuentes</p>
                         <p>Términos y condiciones</p>
                         <a href="">Contacto</a>
                     </Col>
--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
-const About = () => {
+import './aboutStyle.css';
+
+const sections = [
+    {
+        title: "Catálogo Extenso y Diversificado",
+        content: "Bills TV te ofrece una biblioteca en constante expansión con miles de títulos disponibles. Disfruta de una variada selección que incluye: Películas de Estreno, Series Originales, Documentales, y Contenido Infantil."
+    },
+    {
+        title: "Alta Calidad de Transmisión",
+        content: "Experimenta el mejor entretenimiento con calidad de imagen y sonido excepcionales. Bills TV ofrece transmisiones en 4K Ultra HD y HDR para una visualización nítida y vívida, junto con audio envolvente Dolby Atmos."
+    },
+    {
+        title: "Plataforma Intuitiva y Personalizable",
+        content: "Nuestra interfaz amigable y fácil de usar permite una navegación sin esfuerzo. Además, Bills TV aprende de tus hábitos de visualización para ofrecerte recomendaciones personalizadas."
+    },
+    {
+        title: "Multidispositivo y Sin Conexión",
+        content: "Disfruta de Bills TV en cualquier momento y lugar. Compatible con televisores inteligentes, tabletas, móviles, consolas de videojuegos y computadoras personales. Descarga tus series y películas favoritas para verlas sin conexión."
+    },
+    {
+        title: "Planes Flexibles",
+        content: "Bills TV ofrece distintos planes de suscripción para adaptarse a tus necesidades: Plan Básico, Plan Premium y Plan Ultimate."
+    },
+    {
+        title: "Contenido Sin Anuncios",
+        content: "Disfruta de una experiencia de visualización ininterrumpida con nuestra política de cero anuncios. En Bills TV, tu entretenimiento es lo primero."
+    },
+    {
+        title: "Contenido Internacional",
+        content: "Sumérgete en una amplia selección de contenido internacional, con series y películas de todo el mundo. Explora diferentes culturas y estilos narrativos sin salir de casa."
+    }
+];
+
+function About() {
+
+
+
+
     return (
-        <div>
-            About
+        <div className="about-container">
+            <h1 >Bills TV: Tu Destino de Streaming de Series y Películas</h1>
+            {sections.map((section, index) => (
+                <div key={index} className="section">
+                    <h2>{section.title}</h2>
+                    <p>{section.content}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default About
+export default About;
+
